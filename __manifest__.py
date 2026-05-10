f916b37c2a06484d70e2dedce9d7eeade184e8f9
RahulTS1989 <148099345+RahulTS1989@users.noreply.github.com>
2026-05-10 16:07:18 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Traveloop",
+    "version": "1.0.0",
+    "summary": "Smart multi-city travel planning platform",
+    "category": "Travel",
+    "license": "LGPL-3",
+    "depends": ["base", "web"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/menu.xml",
+        "views/dashboard_views.xml",
+        "views/trip_views.xml",
+        "views/itinerary_views.xml",
+        "views/budget_views.xml",
+        "views/packing_views.xml",
+        "views/notes_views.xml",
+        "views/public_templates.xml",
+        "views/portal_assets.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
+
